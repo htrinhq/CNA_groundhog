3eb7699d89479d3625c8eab378c5727652966fb1
Hugo TRINH <hugo.trinh-quy@epitech.eu>
2019-01-31 10:57:55 +0100

    get the PEP8 coding style

--- display.py
+++ display.py
@@ -1,31 +1,32 @@
-#Display functions for GroundHog
+#Display functions for GroundHog.
 
 from stats import *
 
-def display(temperatures, period):
-    #Display Stats about Temperatures
-    r = tempIncreaseAvg(temperatures, period)
-    g = relativeTempEv(temperatures, period)
-    s = standardDeviation(temperatures, period)
-    if getSwitch(r) == True:
+def display(temperatures: [float], period: int):
+    """Display Stats about Temperatures."""
+    r = temp_increase_avg(temperatures, period)
+    g = relative_temp_ev(temperatures, period)
+    s = standard_deviation(temperatures, period)
+    if get_switch(r) == True:
         print("g=" + "{0:.2f}".format(g), "r=" + "{0:.0f}".format(r) + "%",
-              "s=" + "{0:.2f}".format(s), "a switch occurs", sep="\t\t")
+              "s=" + "{0:.2f}".format(s), "a switch occurs", sep="\t")
     else:
         print("g=" + "{0:.2f}".format(g), "r=" + "{0:.0f}".format(r) + "%",
-              "s=" + "{0:.2f}".format(s), sep="\t\t")
+              "s=" + "{0:.2f}".format(s), sep="\t")
 
 
-def chooseDisplay(temperatures, period):
-    #Select correct output to display
+def choose_display(temperatures: [float], period: int):
+    """Select correct output to display."""
     if len(temperatures) < period:
-        print("g=nan", "r=nan%", "s=nan", sep="\t\t")
+        print("g=nan", "r=nan%", "s=nan", sep="\t")
     elif len(temperatures) == period:
         print("g=nan", "r=nan%", "s=" +
-              "{0:.2f}".format(standardDeviation(temperatures, period)), sep="\t\t")
+              "{0:.2f}".format(standard_deviation(temperatures, period)), 
+              sep="\t")
     else:
         display(temperatures, period)
 
 def helper():
-    #Display Help for launch
+    """Display Help for launch."""
     print("SYNOPSIS\n\t./groundhog period\n")
     print("DESCRIPTION\n\tperiod\tthe number of days defining a period")
